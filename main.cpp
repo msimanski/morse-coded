7f8ce54c516cc18e068f035af80af8602e0e57b7
Michael Simanski <mfsimanski@gmail.com>
2018-05-09 17:12:19 -0400

    Finally got filestream working. Added text files and translator class file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  cout << "Hello world"
+  ifstream input("/Users/michaelsimanski/Documents/Projects C++/morse-coded/input.txt"); //init input controller
+  ofstream output("/Users/michaelsimanski/Documents/Projects C++/morse-coded/output.txt"); //init output controller
+
+  if (input.is_open())
+  {
+    cout << "ifstream is open" << "\n";
+  }
+  if (input.good())
+  {
+    cout << "ifstream is good" << "\n";
+  }
+
+  string fileContents = ""; //init message, that will be filled by input.txt
+  string str; //temp string
+
+  cout << "\n";
+
+  while (input >> fileContents)
+  {
+    cout << fileContents << "\n";
+  }
+
+  //cout << "test" << "\n";
+
   return 0;
 }
